c648898db0f8aa415a24c2889a1787af06706637
Alexandr Brik <g00dv1n.private@gmail.com>
2017-03-13 21:23:48 +0200

    update

---
+++ static/js/bootstrap-loader.c403f133cbe2fdf8b58f.js
@@ -0,0 +1,2 @@
+webpackJsonp([2,3],{153:function(t,s,n){t.exports=n.p+"static/fonts/glyphicons-halflings-regular.f4769f9.eot"},255:function(t,s,n){t.exports=n.p+"static/img/glyphicons-halflings-regular.89889688147bd7575d6327160d64e760.svg"},275:function(t,s,n){t.exports=n.p+"static/fonts/glyphicons-halflings-regular.e18bbf6.ttf"},276:function(t,s,n){t.exports=n.p+"static/fonts/glyphicons-halflings-regular.fa27723.woff"},277:function(t,s,n){t.exports=n.p+"static/fonts/glyphicons-halflings-regular.448c34a.woff2"}},[290]);
+//# sourceMappingURL=bootstrap-loader.c403f133cbe2fdf8b58f.js.map
